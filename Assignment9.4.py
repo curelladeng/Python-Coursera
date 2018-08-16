1e871d7e0b1bfc8ccbf70ba10e242d85451c2454
curelladeng <42287996+curelladeng@users.noreply.github.com>
2018-08-15 21:45:49 -0700

    Update Assignment9.4.py

--- Assignment9.4.py
+++ Assignment9.4.py
@@ -1,1 +1,25 @@
+#9.4 Write a program to read through the mbox-short.txt 
+#and figure out who has the sent the greatest number of mail messages. 
+#The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. 
+#The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. 
+#After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+counts = dict()
+for line in handle:
+  line = line.rstrip()
+  if not line.startswith('From '): continue
+  email = line.split()[1]
+  counts[email] = counts.get(email,0) + 1 #get email as dict key, give value 0 then plus 1 to start counting
+
+maxmail = none
+ct = 0
+for email, value in counts.items(): #items() returns a list of dict of (key, value)
+  if value is none or value > ct:
+    maxemail = email
+    ct = value
+print(maxemail, ct)
+  
